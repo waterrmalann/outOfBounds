194d0caa9e4ede411a5222ece4c3c2e8c2524329
Alan <56948680+waterrmalann@users.noreply.github.com>
2023-11-01 00:18:04 +0530

    fixes

--- auth-service/server.js
+++ auth-service/server.js
@@ -1,12 +1,12 @@
 import express from 'express';
-import {dotenv} from 'dotenv';
+import dotenv from 'dotenv';
 dotenv.config();
 
 const app = express();
 
 // code...
 
-const port = 3003;
+const port = 3005;
 app.listen(port, () => {
     console.log(`[ SERVICE :: AUTH ] Authentication Service is listening on http://localhost:${port}`);
 });
