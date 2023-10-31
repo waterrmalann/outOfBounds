267b2a67609a68a38a2c3e9df095be4fa32449a4
Alan <56948680+waterrmalann@users.noreply.github.com>
2023-11-01 03:19:08 +0530

    use http-proxy-middleware instead

--- gateway/server.js
+++ gateway/server.js
@@ -1,46 +1,61 @@
 import express from 'express';
-import httpProxy from 'http-proxy';
+import {createProxyMiddleware} from 'http-proxy-middleware';
 import dotenv from 'dotenv';
+import cors from 'cors';
 dotenv.config();
 
 const app = express();
-
-const proxy = httpProxy.createProxyServer({});
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
 const services = {
   auth: process.env.AUTH_SERVICE,
   threads: process.env.THREADS_SERVICE,
   comments: process.env.COMMENTS_SERVICE,
-  queries: process.env.QUERY_SERVICE
+  query: process.env.QUERY_SERVICE
 };
 
 // Middleware for route-specific authentication
 function authenticate(req, res, next) {
-    req.authorized = true;
+    // req.authorized = true;
+    // next();
+  if (req.headers.authorization === 'auth_token') {
     next();
-//   if (req.headers.authorization === 'yourAuthToken') {
-//     next();
-//   } else {
-//     res.status(401).send('Unauthorized');
-//   }
+  } else {
+    res.status(401).send('Unauthorized');
+  }
 }
 
-// Route requests to the appropriate service with authentication middleware
-app.all('/thread/*', authenticate, (req, res) => {
-  proxy.web(req, res, { target: services.threads });
+app.get('/status', (req, res) => {
+  res.send(":: API Gateway is up and running.")
 });
 
-app.all('/comment/*', authenticate, (req, res) => {
-  proxy.web(req, res, { target: services.comments });
+// Create proxy middleware for the Thread Service
+const threadServiceProxy = createProxyMiddleware('/thread', {
+  target: services.threads,
+  changeOrigin: true,
+  pathRewrite: {'^/thread': ''}
 });
 
-app.all('/query/*', authenticate, (req, res) => {
-  proxy.web(req, res, { target: services.query });
+// Create proxy middleware for the Comment Service
+const commentServiceProxy = createProxyMiddleware('/comment', {
+  target: services.comments,
+  changeOrigin: true,
+  pathRewrite: {'^/comment': ''}
 });
 
-app.all('/auth/*', (req, res) => {
-  proxy.web(req, res, { target: services.auth });
+// Create proxy middleware for the Query Service
+const queryServiceProxy = createProxyMiddleware('/query', {
+  target: services.query,
+  changeOrigin: true,
+  pathRewrite: {'^/query': ''}
 });
+
+// Apply the proxy middleware to relevant routes
+app.use('/thread', threadServiceProxy);
+app.use('/comment', commentServiceProxy);
+app.use('/query', queryServiceProxy);
 
 const port = 3000;
 app.listen(port, () => {
